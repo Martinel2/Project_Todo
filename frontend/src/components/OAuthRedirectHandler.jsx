1c6e6501dbe7e3025dbe51880a464be8037cd3e5
Martinel2 <kkuldangi3@naver.com>
2025-02-17 18:23:40 +0900

    notion식으로 코드가 작동하도록 변경 - 프론트에서는 많은 변화가 진행됨. 핸들러 진행 후 프로필이 아닌 홈화면으로 가도록 변경. 노션과 같이 빈곳을 클릭하면 입력할 수 있는 공간이 생기며, 여기에 내용을 작성하면 저장됨. 내용이 비엇다면 제거되도록 함.

--- frontend/src/components/OAuthRedirectHandler.jsx
+++ frontend/src/components/OAuthRedirectHandler.jsx
@@ -16,7 +16,7 @@ function OAuthRedirectHandler({ setToken }) {
       localStorage.setItem("token", token);
       console.log("토큰 설정됨:", token);
       // 프로필 페이지로 리다이렉트
-      navigate("/profile");
+      navigate("/");
     } 
   }, [navigate, setToken]);
 
